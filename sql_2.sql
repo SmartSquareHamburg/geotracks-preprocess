d05b9ba51644e92df81fd037083f52aa91644c0d
thmensing <43534362+thmensing@users.noreply.github.com>
2020-01-27 10:50:13 +0100

    Update sql_2.sql

--- sql_2.sql
+++ sql_2.sql
@@ -1,3 +1,4 @@
 UPDATE cam_pixelpoints SET camname = STDIN1 WHERE camname IS NULL;
 UPDATE cam_pixelpoints SET timestamp = STDIN2 WHERE timestamp IS NULL;
 UPDATE cam_pixelpoints SET geom = ST_SETSRID(ST_MAKEPOINT(x, y), 25832) WHERE geom IS NULL;
+CREATE INDEX cam_pixelpts_idx ON cam_pixelpoints USING GIST(geom);
